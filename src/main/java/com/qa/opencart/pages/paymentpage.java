e0512266bfdb1642bd89a705bb78f4e7095351de
AKSHAY Y. <akshayyerawar80@gmail.com>
2024-05-01 00:42:44 +0530

    corrections done and locator added

--- src/main/java/com/qa/opencart/pages/paymentpage.java
+++ src/main/java/com/qa/opencart/pages/paymentpage.java
@@ -1,8 +1,14 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class paymentpage {
 	
 	
 	int i=10;
+	private By name= By.id("test");
 
+	public void getname() {
+		System.out.println("name");
+	}
 }
